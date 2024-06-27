443d39e41743278f5f50c6faf8b92bee5e2ff8cb
ninamihaylova <163305180+ninamihaylova@users.noreply.github.com>
2024-06-27 12:23:17 +0300

    Initial commit

---
+++ BonusScore/Program.cs
@@ -0,0 +1,26 @@
+﻿class BonusScore
+{
+    public static void Main(String[] args)
+    {
+
+
+        int points = int.Parse(Console.ReadLine());
+
+        double bonus = 0;
+        
+        if (points >= 0 && points <= 3)
+        {
+            bonus =5+points;
+        }
+        else if (points >= 4 && points < 7)
+        {
+            bonus=15+points;
+           
+        }
+        else if (points>6 && points<10)
+        {
+            bonus=20+points;
+        }
+        Console.WriteLine(bonus);
+    }
+}
